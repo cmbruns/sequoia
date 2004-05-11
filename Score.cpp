1a68c4d8b60c3532abbfeda1fd013e5437a00a1a
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-11 20:33:47 +0000

    test commit of Score.cpp
    git-svn-id: https://simtk.org/svn/sequoia/trunk@6 3da64b0a-0202-0410-8c91-c753549fdeb7

---
+++ Score.cpp
@@ -0,0 +1,27 @@
+#include "Score.h"
+#include "MutationMatrix.h"
+
+// $Id$
+
+float conservidue_score(const Conservidue & c1, const Conservidue & c2) {
+	// Simple sum of pairs formula
+	float answer = 0.0;
+	
+	map<char, float>::const_iterator res1;
+	map<char, float>::const_iterator res2;
+	for (res1 = c1.residue_counts.begin(); 
+		 res1 != c1.residue_counts.end();
+		 res1 ++) {
+		for (res2 = c2.residue_counts.begin(); 
+			 res2 != c2.residue_counts.end();
+			 res2 ++) {
+		}
+		float sequence_count = res1->second * res2->second;
+		float bit_score = blosum62.get_score(res1->first, res2->first);
+
+		answer += sequence_count * bit_score;
+	}
+	
+	return answer;
+}
+
