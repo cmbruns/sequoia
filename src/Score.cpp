b725ae95a4cc8eb219ad488f4380f172244fb63c
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-14 15:19:03 +0000

    changed name of conservidue_score to conservidue_pair_score to avoid conflict
    changed scoring loop, because nested map iterators were failing strangely
    
    
    git-svn-id: https://simtk.org/svn/sequoia/trunk@11 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/Score.cpp
+++ src/Score.cpp
@@ -1,23 +1,46 @@
 #include "Score.h"
 #include "MutationMatrix.h"
 
-float conservidue_score(const Conservidue & c1, const Conservidue & c2) {
+float conservidue_pair_score(const Conservidue & c1, const Conservidue & c2) {
 	// Simple sum of pairs formula
 	float answer = 0.0;
+
+	// nested loops of map iterators seem to cause problems on the Mac
+	// So store the c2 keys in a vector
+	vector<char> c2_chars;
+	map<char,float>::const_iterator res2; // Is the nested loop a problem?
+	for (res2 = c2.residue_counts.begin(); 
+		 res2 != c2.residue_counts.end();
+		 res2 ++) {
+		c2_chars.push_back(res2->first);
+	}
 	
-	map<char, float>::const_iterator res1;
-	map<char, float>::const_iterator res2;
+	// nested loops of map iterators seem to cause problems on the Mac
+	// So store the c2 keys in a vector
+	vector<char> c1_chars;
+	map<char,float>::const_iterator res1; // Is the nested loop a problem?
 	for (res1 = c1.residue_counts.begin(); 
 		 res1 != c1.residue_counts.end();
 		 res1 ++) {
-		for (res2 = c2.residue_counts.begin(); 
-			 res2 != c2.residue_counts.end();
-			 res2 ++) {
-		}
-		float sequence_count = res1->second * res2->second;
-		float bit_score = blosum62.get_score(res1->first, res2->first);
+		c1_chars.push_back(res1->first);
+	}
+	
+	vector<char>::const_iterator charit1;
+	for (charit1 = c1_chars.begin(); charit1 != c1_chars.end(); charit1++) {
+			
+		vector<char>::const_iterator charit2;
+		for (charit2 = c2_chars.begin(); charit2 != c2_chars.end(); charit2++) {
+			
+			const char char1 = *charit1;
+			const char char2 = *charit2;
+			const float weight1 = c1.residue_counts[char1];
+			const float weight2 = c2.residue_counts[char2];
+		
+			float sequence_count = weight1 * weight2;
+			float bit_score = blosum62.get_score(char1, char2);
 
-		answer += sequence_count * bit_score;
+			answer += sequence_count * bit_score;
+		}
 	}
 	
 	return answer;
