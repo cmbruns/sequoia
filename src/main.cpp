a8c34d888164c52b5c3805a87cd6935067beab9f
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-19 01:07:40 +0000

    added output of sequences and alignments to the console changed align subroutine call to new member function
    git-svn-id: https://simtk.org/svn/sequoia/trunk@25 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,18 @@ using namespace std;
 int main (int argc, char * const argv[]) {
 
 	BioSequence s1 = "A";
-	BioSequence s2 = "MMA";
+	BioSequence s2 = "A";
+	
+	cout << s1 << endl;
+	cout << s2 << endl;
 	
 	SequenceAlignment a1 = s1;
 	SequenceAlignment a2 = s2;
 	
-	cout << align(a1, a2);
+	cout << a1 << endl;
+	cout << a2 << endl;
+	
+	cout << a1.align(a2);
 	
     return 0;
 }
