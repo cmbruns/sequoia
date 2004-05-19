9562147be0760f355decb932482b6cc6414beaf9
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-19 00:34:54 +0000

    Moved align subroutine to be member function of SequenceAlignment object Created align_profiles subroutine as a wrapper for the member function
    git-svn-id: https://simtk.org/svn/sequoia/trunk@15 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/AlignmentMethod.h
+++ src/AlignmentMethod.h
@@ -3,12 +3,13 @@
 
 // Header for alignment methods used to align objects defined in SequenceAlignment.h
 
+class SequenceAlignment;
+class ConservidueAlignment; // single cell of dynamic programming table
+
 #include "SequenceAlignment.h"
 
 // Basic sequence alignment rountine, for a single step of progressive alignment
 // Should this be a member function of some class?
-SequenceAlignment align(const SequenceAlignment & seq1, const SequenceAlignment & seq2);
-
-class ConservidueAlignment; // single cell of dynamic programming table
+SequenceAlignment align_profiles(const SequenceAlignment & seq1, const SequenceAlignment & seq2);
 
 #endif
