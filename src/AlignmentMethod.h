d6576e069474a15c80f2358b5fc6061dd80ee359
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-23 23:54:26 +0000

    Added AlignmentGranularity enum to distinguish global vs. local alignment Added granularity optional argument to align_profiles
    git-svn-id: https://simtk.org/svn/sequoia/trunk@37 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/AlignmentMethod.h
+++ src/AlignmentMethod.h
@@ -3,13 +3,23 @@
 
 // Header for alignment methods used to align objects defined in SequenceAlignment.h
 
-class SequenceAlignment;
+enum AlignmentGranularity {
+	ALIGN_GLOBAL, // Needleman-Wunsch style
+	ALIGN_LOCAL // Smith-Waterman style
+};
+
+// declaration needed only so it can be called friend to SequenceAlignment
 class ConservidueAlignment; // single cell of dynamic programming table
 
 #include "SequenceAlignment.h"
 
 // Basic sequence alignment rountine, for a single step of progressive alignment
-// Should this be a member function of some class?
-SequenceAlignment align_profiles(const SequenceAlignment & seq1, const SequenceAlignment & seq2);
+// Should this be a member function of some class? -> it is now, SequenceAlignment
+// but the actual method is defined in AlignmentMethod.cpp
+SequenceAlignment align_profiles(
+	const SequenceAlignment & seq1, 
+	const SequenceAlignment & seq2,
+	AlignmentGranularity granularity = ALIGN_GLOBAL
+								 );
 
 #endif
