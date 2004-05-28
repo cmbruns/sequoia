dcb6b8fe54805b175df6394b76ee59c02a188878
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-28 19:34:52 +0000

    Match and extension components of sum-of-pairs are correct in profile alignment. Open, close and Delete components are still lower bounds AlignmentGranularity redesigned for independent specification of global/local and divergent alignment types
    git-svn-id: https://simtk.org/svn/sequoia/trunk@40 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/AlignmentMethod.h
+++ src/AlignmentMethod.h
@@ -3,10 +3,25 @@
 
 // Header for alignment methods used to align objects defined in SequenceAlignment.h
 
-enum AlignmentGranularity {
-	ALIGN_GLOBAL, // Needleman-Wunsch style
-	ALIGN_LOCAL // Smith-Waterman style
+// How to model regions of no relationship between two sequences
+enum DivergenceType {
+	NO_DIVERGENCE, // No special model
+	PAIR_DIVERGENCE, // Explicit alignment step for pairs of unalignable residues
+	GAP_DIVERGENCE // Model divergence using free pass between inserts and deletes
 };
+
+class AlignmentGranularity {
+public:
+	bool align_global; // true if Needleman-Wunsch, false if Smith-Waterman
+	DivergenceType use_divergence; // Permit unaligned pairs?  LOBAL
+	bool branch_alignment; // like POA
+	
+	AlignmentGranularity() :
+		align_global(true), use_divergence(NO_DIVERGENCE), branch_alignment(false)
+	{}
+};
+
+extern AlignmentGranularity default_granularity;
 
 // declaration needed only so it can be called friend to SequenceAlignment
 class ConservidueAlignment; // single cell of dynamic programming table
@@ -14,12 +29,11 @@ class ConservidueAlignment; // single cell of dynamic programming table
 #include "SequenceAlignment.h"
 
 // Basic sequence alignment rountine, for a single step of progressive alignment
-// Should this be a member function of some class? -> it is now, SequenceAlignment
-// but the actual method is defined in AlignmentMethod.cpp
+// This is a wrapper for SequenceAlignment::align()
 SequenceAlignment align_profiles(
 	const SequenceAlignment & seq1, 
 	const SequenceAlignment & seq2,
-	AlignmentGranularity granularity = ALIGN_GLOBAL
+	AlignmentGranularity granularity = default_granularity
 								 );
 
 #endif
