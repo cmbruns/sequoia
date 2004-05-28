24da2486fa8640f324f73a3ab23d7dca4a321949
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-05-28 20:06:38 +0000

    Match and extension components of sum-of-pairs are correct in profile alignment. Open, close and Delete components are still lower bounds More changes to test sum of pairs examples Testing changes This no longer compiles due to some prebindings, though fasta2align does compile.  Probably has to do with the difference in compilation parameters
    git-svn-id: https://simtk.org/svn/sequoia/trunk@48 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <fstream>
-#include "AlignmentMethod.h"
-#include "BioSequence.h"
+
+// #include "AlignmentMethod.h"
+// #include "BioSequence.h"
+// #include "Exceptions.h"
 
 using namespace std;
 
 int main (int argc, char * const argv[]) {
 
-	BioSequence s2 = "WWWWWWWWWWAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCCCCCCCCC";
-	BioSequence s1 = "WWWWWWWWWWCCCCCCCCCC";
+	cout << "test" << endl;
 	
-	SequenceAlignment a1 = s1;
-	SequenceAlignment a2 = s2;
+	// BioSequence s1 = "A";
+	// BioSequence s2 = "A";
 	
-	cout << a1.align(a2);
+	// cout << s1 << endl;
+	
+    // SequenceAlignment a1 = s1;
+	// SequenceAlignment a2 = s2;
+	
+	// cout << "Aligning..." << endl;
+	
+	// SequenceAlignment align = a1.align(a2);
+	
+	// cout << align << endl;
+	// cout << "Sum of pairs score = " << align.sum_of_pairs_score() << endl;
 	
     return 0;
 }
