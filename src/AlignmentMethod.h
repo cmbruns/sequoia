c6b45b90252cdea0a295749a39b5bd640258a19f
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-06-04 19:01:22 +0000

    Added GPL header
    git-svn-id: https://simtk.org/svn/sequoia/trunk@51 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/AlignmentMethod.h
+++ src/AlignmentMethod.h
@@ -1,3 +1,30 @@
+// This file is part of the Sequoia package for macromolecular 
+//  sequence/structure analysis
+// Copyright (C) 2004  Christopher M. Bruns, Ph.D.
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// See the accompanying file 'LICENSE' for details
+// 
+// To contact the author, write to cmbruns@comcast.net or bruns@scripps.edu
+// In publications please cite: Bruns et al (1999), J.Mol.Biol. 288:427-439
+// Please submit bug reports at http://bruns.homeip.net/bugzilla/
+// 
+// To obtain a non-GPL version of this program, see http://bruns.homeip.net/sequoia.html
+
+
 #ifndef __ALIGNMENT_METHOD_H__
 #define __ALIGNMENT_METHOD_H__
 
@@ -16,6 +43,7 @@ public:
 	DivergenceType use_divergence; // Permit unaligned pairs?  LOBAL
 	bool branch_alignment; // like POA
 	
+	// Set default alignment method here
 	AlignmentGranularity() :
 		align_global(true), use_divergence(NO_DIVERGENCE), branch_alignment(false)
 	{}
