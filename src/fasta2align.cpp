d9709163e37ee1d9a7598f2d4e46da530b34d940
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-06-04 19:32:24 +0000

    Updated GPL header
    Changed for initial testing of structure accessible surface area computation.  This has been extended in residue_area.cpp
    
    
    git-svn-id: https://simtk.org/svn/sequoia/trunk@63 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/fasta2align.cpp
+++ src/fasta2align.cpp
@@ -1,3 +1,40 @@
+// This file is part of the Sequoia package for macromolecular 
+//  sequence/structure analysis
+// Copyright (C) 2004  Christopher M. Bruns, Ph.D.
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// See the accompanying file 'LICENSE' for details
+// 
+// To contact the author, write to cmbruns@comcast.net or bruns@scripps.edu
+// In publications please cite: Bruns et al (1999), J.Mol.Biol. 288:427-439
+// Please submit bug reports at http://bruns.homeip.net/bugzilla/
+// 
+// To obtain a non-GPL version of this program, see http://bruns.homeip.net/sequoia.html
+//
+
+// $Id$
+// $Header$
+// $Log$
+// Revision 1.5  2004/06/04 19:32:24  cmbruns
+// Updated GPL header
+//
+// Changed for initial testing of structure accessible surface area computation.  This has been extended in residue_area.cpp
+//
+
+
 #include <iostream>
 #include <fstream>
 #include "AlignmentMethod.h"
@@ -8,69 +45,47 @@ using namespace std;
 
 int main (int argc, char * const argv[]) {
 
-	BioSequence s1 = "AAWWAACCAA";
-	BioSequence s2 = "WWCC";
-	BioSequence s3 = "HHHMITNLERVGHPKERCFTF";
-	SequenceAlignment align1 = s1;	
-	SequenceAlignment align2 = s2;
-	SequenceAlignment align3 = s3;
-	align1.set_weight(0.20);
-	align2.set_weight(1.65);
-	align3.set_weight(0.96);
+	// TODO - make this a command line parameter
+	const char * = "pdb1coa.ent";
 	
-	SequenceAlignment true_alignment;
-	true_alignment.load_fasta_file("fnr_pdr_true.fasta");
-	// cout << true_alignment << endl;
+	PDBEntry test_pdb;
+	test_pdb.load_pdb_file(pdb_file_name);
+	
+	PDBModel & pdb_model = test_pdb.get_first_model();
+	for (char chain_id = pdb_model.chain_order().begin();
+		 chain_id != pdb_modle.chain_order().end();
+		 chain_id ++) {
+		
+		PDBChain & chain = pdb_model.chain(chain_id);
 
-	// Read first sequence file
+		chain.store_accessible_surface_area();
+		for (unsigned int r = 0; r < chain.residue_count(); r++) {
+			const PDBResidue & residue = chain.get_residue(r);
+			
+			double residue_area = 0.0;
+			try {residue_area = residue.get_accessible_surface_area();}
+			catch (AREA_NOT_YET_COMPUTED_EXCEPTION e) {continue;}
+			
+			cout << residue.one_letter_code() << " ";
+			cout << residue.get_residue_number() << " ";
+			cout << residue_area << endl;
+		}
+	}
+		
+	return 0;
+	
 	SequenceAlignment a1;
-	a1.load_fasta_file("fnr.fasta");
+	a1.load_pdb_file("1FND.pdb");
 	a1.set_weight(1.0);
-
+	
+	cout << a1 << endl;
+	
 	// Read second sequence file
 	SequenceAlignment a2;
 	a2.load_fasta_file("pdr.fasta");
-	a2.set_weight(1.0);
+	a2.set_weight(0.7);
 	
-	SequenceAlignment a3;
-	a3.load_fasta_file("b5.fasta");
-	a3.set_weight(1.0);
+	cout << a1.align(a2);
 	
-	AlignmentScore cumulative_alignment_score;
-	
-	SequenceAlignment alignment;
-	alignment = a1.align(a2);
-	AlignmentScore SOP = alignment.sum_of_pairs_score();
-	// align1.print_debug(cout);
-	// align2.print_debug(cout);
-	// alignment.print_debug(cout);
-
-	cout << alignment << endl;
-	cout << "Sum of pairs score = " << SOP << endl;
-	SOP.print_details(cout);
-	cumulative_alignment_score += alignment.alignment_score();
-	cout << "Cumulative alignment score = " << cumulative_alignment_score << endl;
-	cumulative_alignment_score.print_details(cout);
-	cout << "Discrepancy = " << SOP - cumulative_alignment_score << endl;
-	cout << endl << endl;
-	
-	// alignment.print_debug(cout);
-	// align3.print_debug(cout);
-	alignment = a3.align(alignment); // Add third sequence to alignment
-	SOP = alignment.sum_of_pairs_score();
-	// alignment.print_debug(cout);
-
-	cout << alignment << endl;
-	cout << "Sum of pairs score = " << SOP << endl;
-	SOP.print_details(cout);
-	cumulative_alignment_score += alignment.alignment_score();
-	cout << endl << endl;
-	cout << "Cumulative alignment score = " << cumulative_alignment_score << endl;
-	cout << "Discrepancy = " << SOP - cumulative_alignment_score << endl;
-	cumulative_alignment_score.print_details(cout);
-	cout << endl << endl;
-	
-	// cout << "Sensitivity = " << alignment.report_accuracy(true_alignment) << endl;
-
     return 0;
 }
