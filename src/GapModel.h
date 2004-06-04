ef2ca6c6e489bcf2ba2e27ff9dae9fdff4041573
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2004-06-04 19:09:06 +0000

    Updated GPL header
    Change exception syntax to use classes instead of integers
    
    
    git-svn-id: https://simtk.org/svn/sequoia/trunk@56 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/GapModel.h
+++ src/GapModel.h
@@ -1,11 +1,38 @@
-#ifndef __GAP_MODEL_H__
-#define __GAP_MODEL_H__
-
-// classes and instances for piecewise linear gap penalties
+// This file is part of the Sequoia package for macromolecular 
+//  sequence/structure analysis
+// Copyright (C) 2004  Christopher M. Bruns, Ph.D.
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// 
+// See the accompanying file 'LICENSE' for details
+// 
+// To contact the author, write to cmbruns@comcast.net or bruns@scripps.edu
+// In publications please cite: Bruns et al (1999), J.Mol.Biol. 288:427-439
+// Please submit bug reports at http://bruns.homeip.net/bugzilla/
+// 
+// To obtain a non-GPL version of this program, see http://bruns.homeip.net/sequoia.html
+// 
 
 // $Id$
 // $Header$
 // $Log$
+// Revision 1.3  2004/06/04 19:09:06  cmbruns
+// Updated GPL header
+//
+// Change exception syntax to use classes instead of integers
+//
 // Revision 1.2  2004/05/28 19:39:36  cmbruns
 // Match and extension components of sum-of-pairs are correct in profile alignment.
 // Open, close and Delete components are still lower bounds
@@ -15,6 +42,11 @@
 // Revision 1.1  2004/05/23 23:46:52  cmbruns
 // New structures for piecewise linear gap penalty model
 //
+
+// classes and instances for piecewise linear gap penalties
+
+#ifndef __GAP_MODEL_H__
+#define __GAP_MODEL_H__
 
 #include <vector>
 #include "Exceptions.h"
@@ -50,13 +82,13 @@ public:
 	AlignmentScore total_penalty(unsigned int gap_length) const;
 	unsigned int minimum_gap_length(unsigned int segment) const {return p_segments[segment].minimum_gap_length();}
 	void add_segment(unsigned int min_length, double ext_pen) {
-		if (ext_pen > 0) throw GAP_MODEL_PROBLEM_EXCEPTION;
+		if (ext_pen > 0) throw GAP_MODEL_PROBLEM_EXCEPTION();
 		if (p_segments.size() > 0) {
 			// Sanity check
 			unsigned int previous_segment_min_length = p_segments.back().minimum_gap_length();
 			double previous_extension_penalty = p_segments.back().extension_penalty();
-			if (ext_pen < previous_extension_penalty) throw GAP_MODEL_PROBLEM_EXCEPTION;
-			if (min_length < previous_segment_min_length) throw GAP_MODEL_PROBLEM_EXCEPTION;
+			if (ext_pen < previous_extension_penalty) throw GAP_MODEL_PROBLEM_EXCEPTION();
+			if (min_length < previous_segment_min_length) throw GAP_MODEL_PROBLEM_EXCEPTION();
 		}
 		GapSegment new_segment(min_length, ext_pen);
 		p_segments.push_back(new_segment);
@@ -94,7 +126,7 @@ public:
 	AlignmentScore extension_penalty(unsigned int gseg) const {return sequence_weight * p_gap_model->extension_penalty(gseg);}
 	// Merge together two profiles during alignment
 	ResidueGapParameter combine(const ResidueGapParameter & rgp2) const {
-		if (p_gap_model != rgp2.p_gap_model) throw GAP_MODEL_PROBLEM_EXCEPTION;
+		if (p_gap_model != rgp2.p_gap_model) throw GAP_MODEL_PROBLEM_EXCEPTION();
 		ResidueGapParameter answer = *this;
 		answer.sequence_weight += rgp2.sequence_weight;
 		for (unsigned int gseg = 0; gseg < p_gap_model->segment_count(); gseg ++) {
