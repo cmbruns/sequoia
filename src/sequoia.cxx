7dfa429766f8583907b1647688e0dc5b58e116e5
cmbruns <cmbruns@3da64b0a-0202-0410-8c91-c753549fdeb7>
2005-12-07 03:11:12 +0000

    Change copyright date in initial screen. Change mac ncurses lib to libncurses.5, for backward compatibility.
    git-svn-id: https://simtk.org/svn/sequoia/tags/rel0_9_9@90 3da64b0a-0202-0410-8c91-c753549fdeb7

--- src/sequoia.cxx
+++ src/sequoia.cxx
@@ -32,7 +32,7 @@ int main()
   status_os << "\nSEQUOIA multiple sequence alignment tool\n";
   status_os << "version " << VERSION << "\n";
   status_os << "compiled on " << timestamp << "\n";
-  status_os << "copyright (c) 1995-2003\nby Chris Bruns, Ph.D.\n";
+  status_os << "copyright (c) 1995-2005\nby Chris Bruns, Ph.D.\n";
   status_os << "All rights reserved\n";
   status_os << "\n*** Please see http://bruns.homeip.net/~bruns/scripps/sequoia.html for ***\n";
   status_os << "*** proper citation information. ***\n\n";
